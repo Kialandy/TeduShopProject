f3a3fed8a617d156cdef7240f8cdfa9a9cfdb8e6
ndtc1 <ndtc1@DESKTOP-KCK620H>
2016-06-24 00:39:55 +0700

    Begin with Web Api.

---
+++ TeduShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+
+namespace TeduShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository postCategoryRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+    }
+}
